820661056823ba574839bdc9f077cb924b0a32f5
Tods <trod.dev@gmail.com>
2025-05-31 12:36:09 -0300

    Improving discord_server readability and constants system

--- constants.py
+++ constants.py
@@ -1,19 +1,120 @@
-RIOT_API_TOKEN = 'RGAPI-40348c6b-a483-41e0-a2ec-a6dc25a29787'
-RIOT_PLATFORM = 'br1'
-RIOT_REGION = 'americas'
-DISCORD_TOKEN = 'MTM3MDM3OTg3NTE4NTA2NjAwNA.G4aApZ.Hhv9M5_1kR-mKf6JAFX7eSPIlMfgtmhkV6MDdM'
-FIREBASE_DATABASE_URL = "https://galgos-bet-discord-default-rtdb.firebaseio.com/"
-COMMAND_REGISTER_PLAYER = '!register'
-COMMAND_RANKING = '!ranking'
-COMMAND_BALANCE = "!balance"
-COMMAND_START_BET = '!start'
-COMMAND_SELF_BET = '!self'
-COMMAND_COMMANDS = '!gb_commands'
-TIMEOUT_MESSAGE = "Demorou demais!"
-EVENT_MESSAGE = 'message'
-USER_REF_FIREBASE_DATABASE = 'users'
-USER_REF_ACCOUNTS_FIREBASE_DATABASE = 'accounts'
-USER_REF_POINTS_FIREBASE_DATABASE = 'points'
-USER_REF_PLAYER_NAME_FIREBASE_DATABASE = 'player_name'
-BLUE_SIDE = 100
-RED_SIDE = 200
+import discord
+
+
+class Constants:
+    class Generic:
+        EMPTY_STRING = ""
+        UNKNOWN = "Desconhecido"
+        PLAYER_NAME = "player_name"
+        PLAYER_TAG = "player_tag"
+        PUUID = "puuid"
+        POINTS = "points"
+        ACCOUNT = "account"
+        GAME_ID = "gameId"
+        EVENT_MESSAGE = "message"
+        HASHTAG = "#"
+        PERCENTAGE = "%"
+        INFORMATION = "info"
+        PARTICIPANTS = "participants"
+        WIN = "win"
+        WINS = "wins"
+        LOSSES = "losses"
+        QUEUE_TYPE = "queueType"
+        RANKED_FLEX = "RANKED_FLEX_SR"
+        RANKED_SOLO = "RANKED_SOLO_5x5"
+
+    class Riot:
+        RIOT_API_TOKEN = "RGAPI-40348c6b-a483-41e0-a2ec-a6dc25a29787"
+        RIOT_PLATFORM = "br1"
+        RIOT_REGION = "americas"
+
+    class Discord:
+        DISCORD_TOKEN = "MTM3MDM3OTg3NTE4NTA2NjAwNA.G4aApZ.Hhv9M5_1kR-mKf6JAFX7eSPIlMfgtmhkV6MDdM"
+
+    class Firebase:
+        FIREBASE_DATABASE_URL = "https://galgos-bet-discord-default-rtdb.firebaseio.com/"
+        USER_REF_FIREBASE_DATABASE = "users"
+        USER_REF_ACCOUNTS_FIREBASE_DATABASE = "accounts"
+        USER_REF_POINTS_FIREBASE_DATABASE = "points"
+        USER_REF_PLAYER_NAME_FIREBASE_DATABASE = "player_name"
+
+    class Commands:
+        COMMAND_REGISTER_PLAYER = "!register"
+        COMMAND_RANKING = "!ranking"
+        COMMAND_BALANCE = "!balance"
+        COMMAND_START_BET = "!start"
+        COMMAND_SELF_BET = "!self"
+        COMMAND_COMMANDS = "!gb_commands"
+
+    class CommandsView:
+        GB_COMMANDS = "**!gb_commands:** Comandos gerais do BOT"
+        REGISTER = "**!register:** Se registrar no sistema pela primeira vez"
+        BALANCE = "**!balance:** Saber quantos pontos você tem para apostar"
+        START = "**!start:** Começar o sistema de bet para algum player registrado"
+        SELF = "**!self:** Começar o sistema de bet para sua conta"
+        RANKING = "**!ranking:** Exibe o ranking de pontuação dos membros da season"
+        TITLE = "Comandos"
+
+    class Errors:
+        TIMEOUT_MESSAGE = "Demorou demais!"
+        GALGOS_EXCEPTION = "Ocorreu um erro no sistema de apostas dos Galgos"
+        RANKING_EXCEPTION = "Ocorreu um erro ao recuperar ranking de pontos :"
+        BALANCE_EXCEPTION = "Ocorreu um erro ao recuperar balance de pontos :"
+        RANKING_EXCEPTION_FIREBASE = "Ocorreu um erro ao recuperar pontos do Firebase"
+        UNKNOWN_SEARCH_RESPONSE = "Search response não foi recuperado no formato ideal"
+
+    class Prints:
+        PRINT_REGISTER_PLAYER = "Start player registering for first time"
+        PRINT_BALANCE = "Displaying player points balance"
+        PRINT_START = "Start player bet"
+        PRINT_COMMANDS = "Displaying commands"
+        PRINT_RANKING = "Displaying players points ranking"
+        PRINT_SELF_START = "Start self player bet"
+        MATCH_LIVE = "Partida em andamento"
+
+    class Ranking:
+        RANKING = "Ranking"
+
+    class SelfBet:
+        NOT_FOUND = "Não foi encontrado ninguém com esse nome no banco de dados"
+        DUPLICATED = "Seu usuário está duplicado no banco de dados, por enquanto não suportamos essa função"
+
+    class Bet:
+        WHO_START = ", para quem deseja começar bet (nome da conta)?"
+        NOT_FOUND = "Não foi encontrado ninguém com esse nome no banco de dados"
+        DUPLICATED = "Jogador duplicado no banco de dados, por enquanto não suportamos essa função"
+
+    class BetSystem:
+        STARTING_BET = "Começando bet para "
+        MATCH_FOUND = "Partida encontrada"
+        MATCH_OVER = "Partida finalizada"
+        MATCH_NOT_FOUND_OR_OVER = "Partida não encontrada ou já finalizada"
+        PLAYER = "Jogador "
+        PLAYER_WON = " venceu a partida!"
+        PLAYER_LOST = " perdeu a partida."
+        BET_VIEW_DESCRIPTION_PLAYER_NAME = "**Player:** "
+        BET_VIEW_DESCRIPTION_FLEX_WIN_RATE = "**Flex Win Rate:** "
+        BET_VIEW_DESCRIPTION_SOLO_WIN_RATE = "**Solo Win Rate:** "
+        BET_VIEW_DESCRIPTION_ODDS_WIN = "**Odds Win:** "
+        BET_VIEW_DESCRIPTION_ODDS_LOSE = "**Odds Lose:** "
+        BET_VIEW_TITLE = "Estatísticas de jogador"
+
+    class Register:
+        REGISTER_NICK = ", digite seu nick da main no lol (sem a tag)"
+        REGISTER_TAG = "Agora somente a tag (sem o #)"
+        RIOT_ACCOUNT_NOT_FOUND = "Conta não reconhecida na API Riot, tente novamente"
+        REGISTERED = "Registrado com sucesso"
+
+    class Balance:
+        VIEW_TITLE = "Seus pontos"
+        VIEW_TOTAL_POINTS = "Seu total de pontos é: "
+
+    class Functions:
+        LAZY_DEV = "Já existe uma bet ativa no momento, espere o final ou reclame com o dev que não escalou a aplicação direito por preguiça."
+        NOT_REGISTERED = ", você ainda não possui registro, digite !register primeiro"
+
+    class Colors:
+        BLUE = discord.Color.blue()
+        PURPLE = discord.Color.purple()
+        RED = discord.Color.red()
+        GREEN = discord.Color.green()
--- server/discord/discord_server.py
+++ server/discord/discord_server.py
@@ -1,12 +1,12 @@
 import asyncio
-
 import discord
-from constants import DISCORD_TOKEN, COMMAND_REGISTER_PLAYER, TIMEOUT_MESSAGE, \
-    EVENT_MESSAGE, COMMAND_BALANCE, COMMAND_START_BET, COMMAND_COMMANDS, COMMAND_RANKING, COMMAND_SELF_BET
+
+from constants import Constants
+from exceptions import GalgosBetException
 from modal.account_modal import AccountModal
 from modal.user_modal import UserModal
 from server.firebase.firebase_server import init_firebase, save_user_firebase, get_user_points_firebase, \
-    check_user_registered_firebase, get_account_by_name, add_points_to_user, get_account_by_id, get_points_ranking
+    check_user_registered_firebase, get_account_by_name, get_account_by_id, get_points_ranking
 from server.riot.riot_server import return_account_information, spectate_live_game, check_match_result, \
     retrieve_win_rate
 from view.discord_bet_view import DiscordBetView
@@ -15,52 +15,55 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 is_bet_started = False
 
-
 @client.event
 async def on_ready():
     init_firebase()
     print('Application started')
-
 
 @client.event
 async def on_message(message: discord.Message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(COMMAND_REGISTER_PLAYER):
+    if message.content.startswith(Constants.Commands.COMMAND_REGISTER_PLAYER):
+        print(Constants.Prints.PRINT_REGISTER_PLAYER)
         await register_player(message)
 
-    elif message.content.startswith(COMMAND_BALANCE):
+    elif message.content.startswith(Constants.Commands.COMMAND_BALANCE):
+        print(Constants.Prints.PRINT_BALANCE)
         await get_points_balance(message)
 
-    elif message.content.startswith(COMMAND_START_BET):
+    elif message.content.startswith(Constants.Commands.COMMAND_START_BET):
+        print(Constants.Prints.PRINT_START)
         await start_bet(message)
 
-    elif message.content.startswith(COMMAND_COMMANDS):
+    elif message.content.startswith(Constants.Commands.COMMAND_COMMANDS):
+        print(Constants.Prints.PRINT_COMMANDS)
         await display_commands(message)
 
-    elif message.content.startswith(COMMAND_RANKING):
+    elif message.content.startswith(Constants.Commands.COMMAND_RANKING):
+        print(Constants.Prints.PRINT_RANKING)
         await get_ranking(message)
 
-    elif message.content.startswith(COMMAND_SELF_BET):
+    elif message.content.startswith(Constants.Commands.COMMAND_SELF_BET):
+        print(Constants.Prints.PRINT_SELF_START)
         await start_self_bet(message)
 
-
 async def get_ranking(message: discord.Message):
-    ranking = get_points_ranking()
-    print(ranking)
+    try:
+        ranking = get_points_ranking()
+        if not ranking:
+            raise GalgosBetException(Constants.Errors.RANKING_EXCEPTION_FIREBASE)
 
-    title = "Ranking"
-    description = ""
+        description = Constants.Generic.EMPTY_STRING
 
-    for i, player in enumerate(ranking[:10], start=1):
-        name = player.get("player_name", "Desconhecido")
-        points = player.get("points", 0)
-        description += f"**{i}. {name}** — {points} pontos\n"
-    color = discord.Color.blue()
-    await send_embed_message(message, title, description, color)
-
-
+        for i, player in enumerate(ranking[:20], start=1):
+            name = player.get(Constants.Generic.PLAYER_NAME, Constants.Generic.UNKNOWN)
+            points = player.get(Constants.Generic.POINTS, 0)
+            description += f"**{i}. {name}** — {points} pontos\n"
+        await send_embed_message(message, Constants.Ranking.RANKING, description, Constants.Colors.PURPLE)
+    except Exception as exception:
+        raise GalgosBetException(f"{Constants.Errors.RANKING_EXCEPTION}{str(exception)}")
 
 async def bet_for_myself(message):
     try:
@@ -68,136 +71,133 @@ async def bet_for_myself(message):
         accounts_length = len(accounts)
 
         if accounts_length == 0:
-            await message.channel.send(f"Não foi encontrado ninguém com esse nick no banco de dados")
+            await message.channel.send(Constants.SelfBet.NOT_FOUND)
         elif accounts_length >= 2:
-            await message.channel.send("Usuário duplicado no banco de dados, por enquanto não suportamos essa função")
+            await message.channel.send(Constants.SelfBet.DUPLICATED)
         elif accounts_length == 1:
             await handle_bet_for_specific_player_found(message, accounts)
 
     except asyncio.TimeoutError:
-        await message.channel.send(TIMEOUT_MESSAGE)
-
+        await message.channel.send(Constants.Errors.TIMEOUT_MESSAGE)
 
 async def start_self_bet(message: discord.Message):
+    if await block_unregistered_user(message):
+        return
+
+    if await block_if_bet_started(message):
+        return
+
+    await bet_for_myself(message)
+
+async def block_unregistered_user(message: discord.Message):
+    if not check_user_registered_firebase(message.author.id):
+        await message.channel.send(f"{message.author.display_name}{Constants.Functions.NOT_REGISTERED}")
+        return True
+    return False
+
+async def block_if_bet_started(message: discord.Message):
     global is_bet_started
 
-    if not check_user_registered_firebase(message.author.id):
-        await message.channel.send(f"{message.author.display_name}, você ainda não possui registro, digite !register primeiro.")
-        return
-    else:
-        if is_bet_started:
-            await message.channel.send("Já existe uma bet ativa no momento, espere o final ou reclame com o dev que não escalou a aplicação direito por preguiça.")
-            return
-        else:
-            await bet_for_myself(message)
-
+    if is_bet_started:
+        await message.channel.send(Constants.Functions.LAZY_DEV)
+        return True
+    return False
 
 async def start_bet(message: discord.Message):
-    global is_bet_started
+    if await block_unregistered_user(message):
+        return
 
+    if await block_if_bet_started(message):
+        return
+
+    await bet_for_registered_user(message)
+
+async def bet_for_registered_user(message):
     def check_message(received_message):
         return received_message.author == message.author and received_message.channel == message.channel
 
-    if not check_user_registered_firebase(message.author.id):
-        await message.channel.send(f"{message.author.display_name}, você ainda não possui registro, digite !register primeiro.")
-        return
-
-    if is_bet_started:
-        await message.channel.send("Já existe uma bet ativa no momento, espere o final ou reclame com o dev que não escalou a aplicação direito por preguiça.")
-        return
-    else:
-        await bet_for_registered_user(check_message, message)
-
-
-async def bet_for_registered_user(check_message, message):
     try:
-        await message.channel.send(f"{message.author.display_name}, para quem deseja startar bet (nick da conta)?")
+        await message.channel.send(f"{message.author.display_name}{Constants.Bet.WHO_START}")
 
-        player_response = await client.wait_for(EVENT_MESSAGE, timeout=60, check=check_message)
+        player_response = await client.wait_for(Constants.Generic.EVENT_MESSAGE, timeout=60, check=check_message)
         search_response = get_account_by_name(player_response.content)
         search_response_length = len(search_response)
 
         if search_response_length == 0:
-            await message.channel.send(f"Não foi encontrado ninguém com esse nick no banco de dados")
+            await message.channel.send(Constants.Bet.NOT_FOUND)
         elif search_response_length >= 2:
-            await message.channel.send("Usuário duplicado no banco de dados, por enquanto não suportamos essa função")
+            await message.channel.send(Constants.Bet.DUPLICATED)
         elif search_response_length == 1:
             await handle_bet_for_specific_player_found(message, search_response)
+        else:
+            raise GalgosBetException(Constants.Errors.UNKNOWN_SEARCH_RESPONSE)
 
     except asyncio.TimeoutError:
-        await message.channel.send(TIMEOUT_MESSAGE)
-
+        await message.channel.send(Constants.Errors.TIMEOUT_MESSAGE)
 
 async def handle_bet_for_specific_player_found(message, search_response):
     global is_bet_started
-    player_name = search_response[0]["account"]["player_name"]
-    player_tag = search_response[0]["account"]["player_tag"]
-    player_puuid = search_response[0]["account"]["puuid"]
-    await message.channel.send(f"Começando bet para {player_name}#{player_tag}")
+    player_name = search_response[0][Constants.Generic.ACCOUNT][Constants.Generic.PLAYER_NAME]
+    player_tag = search_response[0][Constants.Generic.ACCOUNT][Constants.Generic.PLAYER_TAG]
+    player_puuid = search_response[0][Constants.Generic.ACCOUNT][Constants.Generic.PUUID]
+
+    await message.channel.send(f"{Constants.BetSystem.STARTING_BET}{player_name}{Constants.Generic.HASHTAG}{player_tag}")
+
     spectate_result = spectate_live_game(player_puuid)
     game_id = 0
+    checked_game_id = 0
+
     if spectate_result is not None:
-        game_id = spectate_result["gameId"]
-        await message.channel.send("Partida encontrada!")
+        game_id = spectate_result[Constants.Generic.GAME_ID]
         is_bet_started = True
-        checked_game_id = 0
+
+        await message.channel.send(Constants.BetSystem.MATCH_FOUND)
 
         await handle_bet_view(message=message, player_name=player_name, puuid=player_puuid)
 
         await asyncio.sleep(60)
+
         while is_bet_started:
             spectate_result = spectate_live_game(player_puuid)
+
             if spectate_result is not None:
-                checked_game_id = spectate_result["gameId"]
+                checked_game_id = spectate_result[Constants.Generic.GAME_ID]
+
             if spectate_result is None or checked_game_id != game_id:
                 is_bet_started = False
-                await message.channel.send("Partida finalizada!")
+                await message.channel.send(Constants.BetSystem.MATCH_OVER)
             else:
-                print("Partida em andamento.")
+                print(Constants.Prints.MATCH_LIVE)
                 await asyncio.sleep(60)
+
     else:
         is_bet_started = False
-        await message.channel.send("Partida não encontrada ou já finalizada!")
+        await message.channel.send(Constants.BetSystem.MATCH_NOT_FOUND_OR_OVER)
 
     if game_id != 0:
         match_result = check_match_result(game_id)
-        for participant in match_result["info"]["participants"]:
-            if participant["puuid"] == player_puuid:
-                result = participant["win"]
+        for participant in match_result[Constants.Generic.INFORMATION][Constants.Generic.PARTICIPANTS]:
+            if participant[Constants.Generic.PUUID] == player_puuid:
+                result = participant[Constants.Generic.WIN]
                 if result:
-                    await message.channel.send(f"Jogador {player_name}#{player_tag} venceu a partida!")
+                    await message.channel.send(f"{Constants.BetSystem.PLAYER}{player_name}{Constants.Generic.HASHTAG}{player_tag}{Constants.BetSystem.PLAYER_WON}")
                 else:
-                    await message.channel.send(f"Jogador {player_name}#{player_tag} perdeu a partida.")
-
-
-def randomize_lost_joke():
-    jokes = [
-        ""
-    ]
-    chosen_joke = []
-    return chosen_joke
-
-
-def randomize_win_joke():
-    jokes = [
-        ""
-    ]
-    chosen_joke = []
-    return chosen_joke
-
+                    await message.channel.send(f"{Constants.BetSystem.PLAYER}{player_name}{Constants.Generic.HASHTAG}{player_tag}{Constants.BetSystem.PLAYER_LOST}")
 
 async def handle_bet_view(message, player_name, puuid):
     match_results = retrieve_win_rate(puuid=puuid)
     flex_rate = 0
     solo_rate = 0
+
     for entry in match_results:
-        if entry["queueType"] == "RANKED_FLEX_SR":
-            flex_wins = entry["wins"]
-            flex_losses = entry["losses"]
+        if entry[Constants.Generic.QUEUE_TYPE] == Constants.Generic.RANKED_FLEX:
+            flex_wins = entry[Constants.Generic.WINS]
+            flex_losses = entry[Constants.Generic.LOSSES]
             flex_rate = flex_wins / (flex_wins + flex_losses) * 100
-        if entry["queueType"] == "RANKED_SOLO_5x5":
-            solo_wins = entry["wins"]
-            solo_losses = entry["losses"]
+
+        if entry[Constants.Generic.QUEUE_TYPE] == Constants.Generic.RANKED_SOLO:
+            solo_wins = entry[Constants.Generic.WINS]
+            solo_losses = entry[Constants.Generic.LOSSES]
             solo_rate = solo_wins / (solo_wins + solo_losses) * 100
 
     house_edge = 0.95
@@ -208,91 +208,91 @@ async def handle_bet_view(message, player_name, puuid):
     odd_lose = round((1 / prob_lose) * house_edge, 2)
 
     description = f"""
-    **Player:** {player_name}
-    **Flex Win Rate:** {flex_rate:.2f}%
-    **Solo Win Rate:** {solo_rate:.2f}%
-    **Odds Win:** {odd_win:.2f}
-    **Odds Lose:** {odd_lose:.2f}
+    {Constants.BetSystem.BET_VIEW_DESCRIPTION_PLAYER_NAME}{player_name}
+    {Constants.BetSystem.BET_VIEW_DESCRIPTION_SOLO_WIN_RATE}{solo_rate:.2f}{Constants.Generic.PERCENTAGE}
+    {Constants.BetSystem.BET_VIEW_DESCRIPTION_FLEX_WIN_RATE}{flex_rate:.2f}{Constants.Generic.PERCENTAGE}
+    {Constants.BetSystem.BET_VIEW_DESCRIPTION_ODDS_WIN}{odd_win:.2f}
+    {Constants.BetSystem.BET_VIEW_DESCRIPTION_ODDS_LOSE}{odd_lose:.2f}
     """
-    await send_embed_message(message, "Estatísticas de jogador", description, discord.Color.blue())
 
+    await send_embed_message(message, Constants.BetSystem.BET_VIEW_TITLE, description, Constants.Colors.BLUE)
 
 async def register_player(message: discord.Message):
     def check_message(received_message):
         return received_message.author == message.author and received_message.channel == message.channel
 
-    if check_user_registered_firebase(message.author.id):
-        await message.channel.send(f"{message.author.display_name}, já possui registro.")
+    if await block_unregistered_user(message):
         return
-    else:
-        response_name = None
-        response_tag = None
-        riot_data = None
 
-        try:
-            account_verified = False
-            while not account_verified:
-                await message.channel.send(f"{message.author.display_name}, digite seu nick da main no lol (sem a tag)")
-                response_name = await client.wait_for(EVENT_MESSAGE, timeout=60, check=check_message)
+    response_name = None
+    response_tag = None
+    riot_data = None
 
-                await message.channel.send("Agora somente a tag (sem o #)")
-                response_tag = await client.wait_for(EVENT_MESSAGE, timeout=60, check=check_message)
+    try:
+        account_verified = False
 
-                riot_data = return_account_information(response_name.content, response_tag.content)
-                if riot_data is None:
-                    await message.channel.send("Conta não reconhecida na API Riot, tente novamente.")
-                else:
-                    account_verified = True
+        while not account_verified:
+            await message.channel.send(f"{message.author.display_name}{Constants.Register.REGISTER_NICK}")
+            response_name = await client.wait_for(Constants.Generic.EVENT_MESSAGE, timeout=60, check=check_message)
 
-            main_account = AccountModal(
-                player_name=response_name.content,
-                player_tag=response_tag.content,
-                main=True,
-                puuid=riot_data["puuid"]
-            )
+            await message.channel.send(Constants.Register.REGISTER_TAG)
+            response_tag = await client.wait_for(Constants.Generic.EVENT_MESSAGE, timeout=60, check=check_message)
 
-            user = UserModal(
-                user_id=message.author.id,
-                name=message.author.name,
-                nick=message.author.nick,
-                accounts=[main_account],
-                registered=True,
-                points=1000.0
-            )
-            save_user_firebase(user)
-            await message.channel.send("Registrado!")
+            riot_data = return_account_information(response_name.content, response_tag.content)
 
-        except asyncio.TimeoutError:
-            await message.channel.send(TIMEOUT_MESSAGE)
+            if riot_data is None:
+                await message.channel.send(Constants.Register.RIOT_ACCOUNT_NOT_FOUND)
+            else:
+                account_verified = True
 
+        main_account = AccountModal(
+            player_name=response_name.content,
+            player_tag=response_tag.content,
+            main=True,
+            puuid=riot_data[Constants.Generic.PUUID]
+        )
+
+        user = UserModal(
+            user_id=message.author.id,
+            name=message.author.name,
+            nick=message.author.nick,
+            accounts=[main_account],
+            registered=True,
+            points=1000.0
+        )
+
+        save_user_firebase(user)
+
+        await message.channel.send(Constants.Register.REGISTERED)
+
+    except asyncio.TimeoutError:
+        await message.channel.send(Constants.Errors.TIMEOUT_MESSAGE)
 
 async def get_points_balance(message: discord.Message):
-    if not check_user_registered_firebase(message.author.id):
-        await message.channel.send(
-            f"{message.author.display_name}, você ainda não possui registro, digite !register primeiro!.")
+    if await block_unregistered_user(message):
         return
 
-    user_points = get_user_points_firebase(message.author.id)
-    if user_points == 0:
-        await send_embed_message(message, "Pontos", f"Seu total de pontos é: {user_points}", discord.Color.red())
+    try:
+        user_points = get_user_points_firebase(message.author.id)
+        if user_points == 0:
+            await send_embed_message(message, Constants.Balance.VIEW_TITLE, f"{Constants.Balance.VIEW_TOTAL_POINTS}{user_points}", Constants.Colors.RED)
 
-    elif user_points != 0:
-        await send_embed_message(message, "Pontos", f"Seu total de pontos é: {user_points}", discord.Color.green())
+        elif user_points != 0:
+            await send_embed_message(message, Constants.Balance.VIEW_TITLE, f"{Constants.Balance.VIEW_TOTAL_POINTS}{user_points}", Constants.Colors.GREEN)
 
+    except GalgosBetException as exception:
+        raise GalgosBetException(f"{Constants.Errors.BALANCE_EXCEPTION}{str(exception)}")
 
 async def display_commands(message: discord.Message):
-    title = "Comandos"
-    description = """
-    **!gb_commands:** Comandos gerais do BOT
-    **!register:** Se registrar no sistema pela primeira vez
-    **!balance:** Saber quantos pontos você tem para apostar
-    **!start:** Começar o sistema de bet para algum player registrado
-    **!self:** Começar o sistema de bet para sua conta
-    **!ranking:** Exibe o ranking de pontuação dos membros da season
+    description = f"""
+    {Constants.CommandsView.GB_COMMANDS}
+    {Constants.CommandsView.REGISTER}
+    {Constants.CommandsView.BALANCE}
+    {Constants.CommandsView.START}
+    {Constants.CommandsView.SELF}
+    {Constants.CommandsView.RANKING}
     """
-    color = discord.Color.blue()
-    await send_embed_message(message, title, description, color)
-
+    await send_embed_message(message, Constants.CommandsView.TITLE, description, Constants.Colors.BLUE)
 
 async def send_embed_message(message, title, description, color, content=None, view=None):
     embed = discord.Embed(
@@ -302,5 +302,4 @@ async def send_embed_message(message, title, description, color, content=None, v
     )
     await message.channel.send(embed=embed, content=content, view=view)
 
-
-client.run(DISCORD_TOKEN)
+client.run(Constants.Discord.DISCORD_TOKEN)
